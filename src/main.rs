f63658cb7f90fdf49f50e1615f32fc94cef54195
Felix Blanchette <124934255+FyfiLoulou@users.noreply.github.com>
2024-12-08 17:21:23 -0500

    rust server done

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
-use std::{fs, io::{BufReader, prelude::*}, net::{TcpListener, TcpStream}};
 use server::ThreadPool;
+use std::{
+    fs,
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -15,9 +19,9 @@ fn main() {
         let buf_reader = BufReader::new(&stream);
         let request_line = buf_reader.lines().next().unwrap().unwrap();
         let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-            ("HTTP/1.1 200 OK", "hello.html")
+            ("HTTP/1.1 200 OK", "public/hello.html")
         } else {
-            ("HTTP/1.1 404 NOT FOUND", "404.html")
+            ("HTTP/1.1 404 NOT FOUND", "public/404.html")
         };
 
         let contents = fs::read_to_string(filename).unwrap();
@@ -27,24 +31,4 @@ fn main() {
 
         stream.write_all(response.as_bytes()).unwrap();
     }
-
-    /*
-    fn slow_handle_connection(mut stream: TcpStream) {
-        let buf_reader = BufReader::new(&stream);
-        let request_line = buf_reader.lines().next().unwrap().unwrap();
-
-        let (status_line, filename) = match &request_line[..] {
-            "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "public/hello.html"),
-            "GET /sleep HTTP/1.1" => {
-                thread::sleep(Duration::from_secs(5));
-                ("HTTP/1.1 404 NOT FOUND", "public/404.html")
-            }
-            _ => ("HTTP/1.1 404 NOT FOUND", "public/404.html")
-        };
-        let contents = fs::read_to_string(filename).unwrap();
-        let length = contents.len();
-        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-        stream.write_all(response.as_bytes()).unwrap();
-    }
-    */
 }
